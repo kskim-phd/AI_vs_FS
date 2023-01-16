742d81d4c68d7deae2a5699f2c9c8d56a98cbe04
Juyoung Hahm <jlh2270@cumc.columbia.edu>
2023-01-16 14:20:31 +0900

    Update main_vnet.py

--- main_vnet.py
+++ main_vnet.py
@@ -6,7 +6,7 @@ import pdb
 import numpy as np
 import time
 
-# os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0,1" # Use GPU 
 # Load data, DCM_AUG_vol.shape = (#,128,256,256,1)
 
 [DCM_vol, GT_vol] = load_pickle(DATA_PATH_A)
@@ -22,7 +22,7 @@ test_data, test_gt = DCM_vol, GT_vol
 
 
 # Train and evaluate the model
-model = V_NET(data_shape=DATA_SHAPE, output=OUTPUT, dim=DIM, kernel_size=KERNEL_SIZE, attention=ATTENTION)
+model = V_NET(data_shape=DATA_SHAPE, output=OUTPUT, dim=DIM, kernel_size=KERNEL_SIZE, attention=ATTENTION) # load model
 model.compile(optimizer=Adam(lr=LEARNING_RATE), loss=LOSS, metrics=METRICS)
 model.summary()
 
@@ -40,6 +40,7 @@ result_path = '/tmp/update_code/test_result/github'
 result_file = '/brainmask_caudate_C.pkl'
 save_pickle(result_path + result_file, [test_result, test_data]) #prediction result, brainmask
 
+#GPU use evalutation
 result = []
 time_data = []
 for i in range(len(test_data)):
@@ -49,7 +50,16 @@ for i in range(len(test_data)):
     total_time = np.array('%.4f' % (end-start),  dtype=np.float64)
     time_data.append(total_time.tolist())
 
-pdb.set_trace()
+#CPU use evalutation
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1" # Use CPU
+result = []
+time_data = []
+for i in range(len(test_data)):
+    start = time.time()
+    result.append([i, ] + model.evaluate(np.expand_dims(test_data[i], axis=0), np.expand_dims(test_gt[i], axis=0)))
+    end = time.time()
+    total_time = np.array('%.4f' % (end-start),  dtype=np.float64)
+    time_data.append(total_time.tolist())
 # save test result as excel
 result_df = pd.DataFrame(result)  
 time_df = pd.DataFrame(time_data)
